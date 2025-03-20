8b5ce139d62ed3fb5828bf14aa6d805a1b6c28a8
eren tetik <131614656+erentetk@users.noreply.github.com>
2025-03-20 23:10:54 +0300

    config/constants.js dosyası eklendi

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+const colors = {
+    primary: '#2196f3',// mavi  chat screen header rengi 
+    secondary: '#111111',
+    border: "#e2e2e2",
+    red: "#f44336",
+    green: "#4CAF50",
+    pink: "#e91e63"
+
+
+}
+
+export { colors };
