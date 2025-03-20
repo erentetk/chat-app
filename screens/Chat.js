baa9d711b3b80f0ab1311772f3cf2a3a40c6b997
eren tetik <131614656+erentetk@users.noreply.github.com>
2025-03-20 23:09:21 +0300

    screens/Chat.js dosyası eklendi

---
+++ screens/Chat.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Chat = () => {
+    return (
+
+        <View style={styles.container}> </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    }
+})
+export default Chat;
